0f4aa4b49a3fe5f16522de39566743e5db5bcb2f
Peter Bui <pbui@nd.edu>
2026-05-31 14:37:30 -0400

    Lecture 01: add Exercise 01-C

---
+++ lecture01/exercise01-C/template.cpp
@@ -0,0 +1,29 @@
+// Exercise 01-C: Count Duplicates
+
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+// Functions
+
+int count_duplicates(vector<int> &numbers) {
+    // TODO
+    return 0;
+}
+
+// Main Execution
+
+int main(int argc, char *argv[]) {
+    // TODO: Read numbers
+
+    // TODO: Count duplicates
+    
+    // TODO: Display total
+
+    return 0;
+}
+
+// vim: set sts=4 sw=4 ts=8 ft=cpp:
---
+++ lecture01/exercise01-C/template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Functions
+
+def count_duplicates(numbers : list[int]) -> int:
+    # TODO
+    return 0
+
+# Main Execution
+
+def main():
+    # TODO: Read numbers
+
+    # TODO: Count duplicates
+
+    # TODO: Display total
+    pass
+
+if __name__ == '__main__':
+    main()
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
