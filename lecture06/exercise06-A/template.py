120c9b1aa3999c0aeb74fb90d3bad9b16f37a46e
Peter Bui <pbui@nd.edu>
2026-06-14 22:41:28 -0400

    Lecture 06: add Exercise 06-A

---
+++ lecture06/exercise06-A/template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Exercise 06-A: Subsets
+
+import sys
+
+# Function
+
+def divisible_subsets(n: int) -> int:
+    # TODO: Determine how many subsets of {0...9} are divisible by N
+    return 0 
+
+# Main execution
+
+def main() -> None:
+    for n in map(int, sys.stdin):
+        print(divisible_subsets(n))
+
+if __name__ == '__main__':
+    main()
