a6f7ef5a3427a954768405104efd23091cb978da
Peter Bui <pbui@nd.edu>
2026-06-14 22:37:07 -0400

    Lecture 05: add permutations

---
+++ lecture05/permutations_backtracking.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from typing import Iterator
+
+# Functions
+
+def permutations(p: str, c: set[str]) -> Iterator[str]:
+    '''
+    p:  Current permutation
+    c:  Set of candidates
+    '''
+    # Base case: no more candidates, so yield permutation
+    if not c:
+        yield p
+
+    # Recursive case
+    else:
+        # For each candidate, add it to permutation and remove it from
+        # candidates
+        for e in sorted(c):
+            yield from permutations(p + e, c.difference({e}))
+
+# Main Execution
+
+def main():
+    for permutation in permutations('', set('ABC')):
+        print(permutation)
+
+if __name__ == '__main__':
+    main()
